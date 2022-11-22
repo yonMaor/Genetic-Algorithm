a620c7fdd36308c723b1dea818ba1110a9a29006
Yonatan Maor <yon.maor@gmail.com>
2022-11-22 16:02:11 +0200

    Fixed some typos, and moved test files to tests folder

--- tests/test_individual.py
+++ tests/test_individual.py
@@ -22,11 +22,13 @@ def individual_any_gen(size):
     parent2 = Individual(size, size, "first_gen", "random_genes", loss_type, mut_rate)
     return Individual(size, size, "general", "random_genes", loss_type, mut_rate, parent1, parent2)
 
-def test_get_individual_data_first_gen(individual_first_gen):
-    assert np.shape(individual_first_gen.data) == (10, 10)
+def test_get_individual_data_first_gen(individual_first_gen, size):
+    assert np.shape(individual_first_gen.data) == (size, size)
 
-def test_get_individual_data_from_parents(individual_any_gen):
-    assert np.shape(individual_any_gen.data) == (10, 10)
+def test_get_individual_data_from_parents(individual_any_gen, size):
+    assert np.shape(individual_any_gen.data) == (size, size)
+
+
 
 
 
